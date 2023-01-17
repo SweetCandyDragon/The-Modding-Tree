a15103208989e212f54c0977ce21f0bf9da49f77
SweetCandyDragon <hn082004@gmail.com>
2023-01-17 13:55:47 -0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Rarity Tree",
+	id: "progressable",
+	author: "SweetCandyDragon",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
