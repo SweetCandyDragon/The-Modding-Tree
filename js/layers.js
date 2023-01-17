1802a87a78b119d75f66ea9ed1e5c8cdf39510d9
SweetCandyDragon <hn082004@gmail.com>
2023-01-17 16:15:43 -0700

    a

--- js/layers.js
+++ js/layers.js
@@ -31,7 +31,7 @@ addLayer("p", {
             title: "Point Boost",
             description: "x2 your point gain.",
             cost(x) { return new Decimal(1).mul(x) },
-            display() { return "Blah" },
+            display() { return "Amount" },
             canAfford() { return player[this.layer].points.gte(this.cost()) },
             buy() {
                 player[this.layer].points = player[this.layer].points.sub(this.cost())
