dc1140d18ae5616db9cbd9d9c655a869a95c6147
SweetCandyDragon <hn082004@gmail.com>
2023-01-17 19:23:25 -0700

    a

--- js/mod.js
+++ js/mod.js
@@ -43,8 +43,10 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
+	if (hasUpgrade('r', 11)) gain = gain.times(2)
 	if (hasUpgrade('p', 11)) gain = gain.times(2)
-	if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12))
+	if (hasUpgrade('r', 12)) gain = gain.times(upgradeEffect('r', 12))
+	if (hasUpgrade('r', 12)) gain = gain.times(upgradeEffect('p', 12))
 	return gain
 }
 
